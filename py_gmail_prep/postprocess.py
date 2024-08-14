52f60828e6e89b5de18f6617a46a963a7b79348e
Mariano Reingart <reingart@gmail.com>
2024-08-14 00:13:12 -0500

    Improve citation detection

--- py_gmail_prep/postprocess.py
+++ py_gmail_prep/postprocess.py
@@ -83,7 +83,6 @@ def parse_gmail(file_path):
 
     msg_str = get_message_body(msg)
     if msg_str:
-        print(file_path, msg_str)
         headers = get_message_headers(msg)
 
         # ignorar mensajes sin publicar
@@ -95,7 +94,7 @@ def parse_gmail(file_path):
 
         timestamp = datetime.datetime.fromtimestamp(float(msg['internalDate'])/1000.).strftime('%Y%m%d_%H%M%S')
         filename = f"{timestamp}_{msg['historyId']}_{msg['id']}.eml"
-        print(filename, clean_body)
+        print(file_path, filename, clean_body)
 
         email = create_rfc822_message(headers, clean_body)
 
@@ -109,58 +108,23 @@ def parse_gmail(file_path):
 def remove_cited_text(body):
     # lista de posibles indicadores de texto citado
     citation_markers = [
-        "On ", "En ", "Le ", "El ", "wrote:", "escribió:", "ha scritto:", "a écrit:", "schrieb:"
+        "On ", "En ", "Le ", "El ", "wrote:", "escribió:", "escribio:", "ha scritto:", "a écrit:", "schrieb:"
     ]
     # dividir el cuerpo en líneas
     lines = body.splitlines()
 
     # recorrer las líneas en orden inverso y buscar un marcador de citación
+    prev = ""
     for i in reversed(range(len(lines))):
         line = lines[i].strip()
         if any(line.startswith(marker) for marker in citation_markers):
-            # si se encuentra un marcador, eliminar todo el texto desde esa línea en adelante
-            return '\n'.join(lines[:i])
+            if any(l.endswith(marker) for marker in citation_markers for l in (line, prev)):
+                # si se encuentra un marcador, eliminar todo el texto desde esa línea en adelante
+                return '\n'.join(lines[:i])
+        prev = line
 
     # si no se encuentra ningún marcador, devolver el cuerpo tal cual
     return body
-
-
-def build_conversations(emails):
-    email_dict = {email['message_id']: email for email in emails}
-    conversations = []
-
-    for response in emails:
-        in_reply_to = response['in_reply_to']
-        print("buscando mensaje orginal para ", in_reply_to)
-        responder = response["from"]
-        if not in_reply_to or not any([address for address in RESPONSE_ADDRESS_FILTERS if address in responder]):
-            print("descartando email pregunta porque no es de una direccion válida", responder)
-            continue
-        if in_reply_to in email_dict:
-            print ("encontrado mensaje original")
-            original = email_dict[in_reply_to]
-            inquirer = original["from"]
-            if any([address for address in RESPONSE_ADDRESS_FILTERS if address in inquirer]):
-                print("descartando email original porque no es de una direccion válida", inquirer)
-                continue
-            question = original['body']
-            answer = response['body']
-            conversations.append({
-                'prompt': question,
-                'completion': answer,
-            })
-            if DEBUG:
-                conversations[-1].update({
-                    'question': original["filename"],
-                    'answer': response["filename"],
-                    'sender': responder,
-                    'from': [address for address in RESPONSE_ADDRESS_FILTERS if address in response["from"]],
-                    'to': [address for address in RESPONSE_ADDRESS_FILTERS if address in original["from"]],
-                    'customer': inquirer,
-                })
-
-
-    return conversations
 
 
 def main():
@@ -171,7 +135,7 @@ def main():
     errores = 0
     ok = 0
     for filename in os.listdir(rfc822_dir):
-        if filename.endswith('.json'):
+        if (filename in sys.argv[1:]) or (not sys.argv[1:] and filename.endswith('.json')):
             file_path = os.path.join(rfc822_dir, filename)
             try:
                 emails.append(parse_gmail(file_path))
